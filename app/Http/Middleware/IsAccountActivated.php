f64352e0adf767dca8d0539a314377bffa9a0551
SokEtak <167615626+SokEtak@users.noreply.github.com>
2025-09-16 09:10:58 +0700

    attemp to fix image url(2 nd attemp)

--- app/Http/Middleware/IsAccountActivated.php
+++ app/Http/Middleware/IsAccountActivated.php
@@ -15,7 +15,7 @@ class IsAccountActivated
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (auth()->user() && auth()->user()->isActive !== 1) {
+        if (auth()->user() && !auth()->user()->isActive ) {
             auth()->logout();
 
             // Redirect with a flash warning instead of status
